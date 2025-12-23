330badb198189f4beffe445948f0491fd4aa3ca4
jonnurik <78033038+jonnurik@users.noreply.github.com>
2025-12-23 22:23:41 +0500

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,1 +1,18 @@
+from db import get_questions
+
+def generate_block(subject, total):
+    """
+    Berilgan fan uchun:
+    oson / o‘rta / qiyin savollardan
+    teng taqsimlab total ta savol qaytaradi
+    """
+
+    part = total // 3
+
+    questions = []
+    questions += get_questions(subject, "oson", part)
+    questions += get_questions(subject, "orta", part)
+    questions += get_questions(subject, "qiyin", part)
+
+    return questions
 
