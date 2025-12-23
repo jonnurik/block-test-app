0d1705a8a76900f5b4662be2056cdda206979446
jonnurik <78033038+jonnurik@users.noreply.github.com>
2025-12-23 22:25:50 +0500

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,18 +1,36 @@
-from db import get_questions
+import random
+import json
+from db import get_conn
 
-def generate_block(subject, total):
-    """
-    Berilgan fan uchun:
-    oson / o‘rta / qiyin savollardan
-    teng taqsimlab total ta savol qaytaradi
-    """
+def get_questions(subject, limit):
+    conn = get_conn()
+    cur = conn.cursor()
 
-    part = total // 3
+    cur.execute("""
+    SELECT question, options, correct
+    FROM questions
+    WHERE subject=?
+    ORDER BY RANDOM()
+    LIMIT ?
+    """, (subject, limit))
 
-    questions = []
-    questions += get_questions(subject, "oson", part)
-    questions += get_questions(subject, "orta", part)
-    questions += get_questions(subject, "qiyin", part)
+    rows = cur.fetchall()
+    conn.close()
 
-    return questions
+    result = []
+    for r in rows:
+        result.append({
+            "question": r[0],
+            "options": json.loads(r[1]),
+            "correct": r[2]
+        })
+    return result
 
+def generate_test(main1, main2):
+    test = []
+    test += get_questions("Ona tili", 10)
+    test += get_questions("Matematika", 10)
+    test += get_questions("Tarix", 10)
+    test += get_questions(main1, 30)
+    test += get_questions(main2, 30)
+    return test
