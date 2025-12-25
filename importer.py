2bcf93e7b9a8298ea3dbaa8d6dad5ad696878ad5
jonnurik <78033038+jonnurik@users.noreply.github.com>
2025-12-25 21:56:16 +0500

    Update importer.py

--- importer.py
+++ importer.py
@@ -1,35 +1,24 @@
 import json
 from db import insert_question
 
+
 def import_json(path, subject, block_type):
-    with open(path, encoding="utf-8") as f:
+    with open(path, "r", encoding="utf-8") as f:
         data = json.load(f)
 
     count = 0
-    for q in data:
-        answers = q.get("answers", [])
-        if len(answers) < 4:
-            continue
-
-        correct = ""
-        for a in answers:
-            if a.get("correct") == "1":
-                correct = a["answer"]
-
-        if not correct:
-            continue
-
-        insert_question((
+    for item in data:
+        insert_question(
             subject,
             block_type,
-            q.get("difficulty", "orta"),
-            q["question"],
-            answers[0]["answer"],
-            answers[1]["answer"],
-            answers[2]["answer"],
-            answers[3]["answer"],
-            correct
-        ))
+            item["question"],
+            item["A"],
+            item["B"],
+            item["C"],
+            item["D"],
+            item["correct"],
+            item.get("difficulty", "o‘rta")
+        )
         count += 1
 
     return count
