82860db1d83279b7fe174defd07fa8bd02a97a23
jonnurik <78033038+jonnurik@users.noreply.github.com>
2025-12-26 09:07:28 +0500

    Update importer.py

--- importer.py
+++ importer.py
@@ -3,22 +3,47 @@ from db import insert_question
 
 
 def import_json(path, subject, block_type):
+    """
+    JSON format:
+    [
+      {
+        "question": "...",
+        "A": "...",
+        "B": "...",
+        "C": "...",
+        "D": "...",
+        "correct": "A",
+        "difficulty": "oson"
+      }
+    ]
+    """
+
     with open(path, "r", encoding="utf-8") as f:
-        data = json.load(f)
+        try:
+            data = json.load(f)
+        except Exception as e:
+            raise ValueError("JSON fayl noto‘g‘ri formatda")
+
+    if not isinstance(data, list):
+        raise ValueError("JSON massiv (list) bo‘lishi kerak")
 
     count = 0
-    for item in data:
-        insert_question(
-            subject,
-            block_type,
-            item["question"],
-            item["A"],
-            item["B"],
-            item["C"],
-            item["D"],
-            item["correct"],
-            item.get("difficulty", "o‘rta")
-        )
-        count += 1
+
+    for i, item in enumerate(data, start=1):
+        try:
+            insert_question(
+                subject,
+                block_type,
+                item["question"],
+                item["A"],
+                item["B"],
+                item["C"],
+                item["D"],
+                item["correct"],
+                item.get("difficulty", "o‘rta")
+            )
+            count += 1
+        except KeyError as e:
+            raise ValueError(f"{i}-savolda maydon yetishmayapti: {e}")
 
     return count
