6ca05954c864fdf1e83cda2fae41ddd582b8f1f8
jonnurik <78033038+jonnurik@users.noreply.github.com>
2025-12-26 20:24:54 +0500

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,8 +1,17 @@
 from db import random_questions
 
 
-def generate(subject, block, count):
-    qs = random_questions(subject, block, count)
-    if len(qs) < count:
-        raise Exception(f"{subject} ({block}) fanidan savol yetarli emas")
-    return qs
+def generate_blocks(asosiy1, asosiy2):
+    blocks = []
+
+    blocks.append(("Ona tili (majburiy)", random_questions("Ona tili", "majburiy", 10)))
+    blocks.append(("Matematika (majburiy)", random_questions("Matematika", "majburiy", 10)))
+    blocks.append(("Tarix (majburiy)", random_questions("Tarix", "majburiy", 10)))
+    blocks.append((asosiy1, random_questions(asosiy1, "asosiy", 30)))
+    blocks.append((asosiy2, random_questions(asosiy2, "asosiy", 30)))
+
+    for title, qs in blocks:
+        if len(qs) == 0:
+            raise Exception(f"{title} fanidan savollar yetarli emas")
+
+    return blocks
